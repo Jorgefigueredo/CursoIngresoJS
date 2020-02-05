1d3a495bc97eb58145ba2a894f3f4c68f78a15d6
Jorgefigueredo <60697610+Jorgefigueredo@users.noreply.github.com>
2020-02-05 10:23:03 -0300

    Solo suma

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,7 +3,19 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	var= primerNumero;
+	var= segundoNumero;
+	var= Resultadosuma;
+
+primerNumero=document.getElementById('primerNumero').value;
+
+primeroNumero=parseInt(primeroNumero);
+segundoNumero=document.getElementById('segundoNumero').value;
+segundoNumero=parseInt(segundoNumero);
+Resultadosuma=primeroNumero+segundoNumero;
+console.log("resultado" +Resultadosuma);
+
+
 	
 }
 
