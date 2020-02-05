2524baa90b3f3d75033d869b4b10545970c536e0
Jorgefigueredo <60697610+Jorgefigueredo@users.noreply.github.com>
2020-02-05 11:42:52 -0300

    Porce4ntaje

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,29 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{ 
+   var sueldo;
+   var aumento;
+   var resultado;
+
+   sueldo=document.getElementById('sueldo').value;
+   sueldo=parseInt(sueldo);
+   
+   aumento=sueldo*10/100;
+   aumento=parseInt(aumento);
+
+   resultado=aumento+sueldo;
+
+   console.log("resultado" +resultado);
+   document.getElementById("Resultado").value = resultado;
+   
+
+
+
+
+
+
+
 
 	
 }
