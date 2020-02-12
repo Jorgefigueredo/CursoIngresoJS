0d3fcc1d0cd959fe1c6ba61fcfbc2fd95733e783
Jorge Figueredo <jorgefigueredo@hotmail.com>
2020-02-12 10:31:58 -0300

    ejercicio 10 if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
+	var nota
+
+	nota=Math.floor(Math.random() * 10) + 1;
+	if (nota>8) 
+	{
+		alert("Excelente");
+	}
+	 else 
+	 	if (nota>3) 
+	{
+		alert("Aprobo");
+	} else 
+	{
+		alert("Vamos la proxima que se puede");
+	}
 	//Genero el número RANDOM entre 1 y 10 
-	
+	console.log(+nota);
 
 }//FIN DE LA FUNCIÓN
