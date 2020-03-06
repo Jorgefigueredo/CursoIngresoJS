a7601141022c943acf9852edf3212c548f1ccbdb
jorge <figueredojorge@outlook.com>
2020-03-05 22:29:11 -0300

    parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,119 @@
+/*Bienvenidos.
+Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a La cantidad de números pares.
+b La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.*/
+
+
 function mostrar()
 {
+	var numero;
+	var letra;
+	var respuesta;
+	var contador;
+	var contadorNumerosPar;
+	var contadorNumerosImpar;
+	var cantidadDeCeros;
+	var sumaDeTodosLosNumerosPositivos;
+	var contadorDeTodosLosNumerosPositivos;
+	var promedioPositivos;
+	var sumaNegativos;
+	var numeroMaximo;
+	var numeroMinimo;
+	var letraMaximo;
+	var letraMinimo;
 
-}
+	respuesta="si";
+	contador=0;
+	contadorNumerosImpar=0;
+	contadorNumerosPar=0;
+	cantidadDeCeros=0;
+	sumaDeTodosLosNumerosPositivos=0;
+	contadorDeTodosLosNumerosPositivos=0;
+	sumaNegativos=0;
+	numeroMaximo=0;
+	numeroMinimo=0;
+
+	while(respuesta=="si")
+	{	
+		do
+		{
+			letra=prompt("Ingrese una letra");
+		}
+		while(!isNaN(letra));
+
+		do
+		{
+			numero=prompt("Ingrese un numero");
+		}
+		while(isNaN(numero)||numero<-100 ||numero>100);
+
+		if (numero%2==0) 
+		{
+			contadorNumerosPar++;
+		}else
+		{
+			contadorNumerosImpar++;
+		}
+
+		if (numero==0) 
+		{
+			cantidadDeCeros++;
+		}
+
+		if (numero>0) 
+		{
+			contadorDeTodosLosNumerosPositivos++;
+			sumaDeTodosLosNumerosPositivos=sumaDeTodosLosNumerosPositivos+numero;
+		}
+		
+		if (numero<0) 
+		{
+			sumaNegativos=sumaNegativos+numero;
+		}
+
+
+		if (contador==0||numero>numeroMaximo) 
+		{
+			numeroMaximo=numero;
+			letraMaximo=letra;
+		}
+
+		if (contador==0||numero<numeroMinimo) 
+		{
+			numeroMinimo=numero;
+			letraMinimo=letra;
+		}
+
+
+
+
+		contador++;
+		respuesta=prompt("Si desea continuar ingrensado datos escibra si");
+	}
+
+	promedioPositivos=sumaDeTodosLosNumerosPositivos/contadorDeTodosLosNumerosPositivos;
+
+		document.write("<br>La cantidad de numeros pares es:  "+contadorNumerosPar);
+		document.write("<br>La cantidad de numeros impares es:   "+contadorNumerosImpar);
+		document.write("<br>La cantidad de ceros es:  "+cantidadDeCeros);
+		document.write("<br>El promedio de todos los numeros positivos ingresados es:   "+promedioPositivos);
+		document.write("<br>La suma de todos los numeros negativos es:  "+sumaNegativos);
+		document.write("<br>El numero y letra del minimo son:  "+numeroMinimo+ " "+letraMinimo);
+		document.write("<br>El numero y letra del maximo son:  "+numeroMaximo+" "+letraMaximo)
+
+
+	}
+
+
+	/*
+La cantidad de numeros pares es: 4
+La cantidad de numeros impares es: 0
+La cantidad de ceros es: 2
+El promedio de todos los numeros positivos ingresados es: 20
+La suma de todos los numeros negativos es: 0-50
+El numero y letra del minimo son: -50d
+El numero y letra del maximo son: 20g*/
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,110 @@
 
+/*Bienvenidos.
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100 (validar),
+la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de temperaturas pares.
+b) La marca del producto más pesado
+c) La cantidad de productos que se conservan a menos de 0 grados.
+d) El promedio del peso de todos los productos.
+f) El peso máximo y el mínimo.*/
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temperatura;
+	var respuesta;
+	var cantidadDeTemperaturaPares;
+	var marcaProductoMasPesado;
+	var pesoMasPesado;
+	var cantidadDeProductosMenos0Grados;
+	var totalPesoTodosLosProductos;
+	var promedioDelPeso;
+	var contadorDePeso;
+	var contador;
+	var pesoMaximo;
+	var pesoMinimo;
 
+
+	respuesta="si";
+	CantidadDeTemperaturaPares=0;
+	pesoMasPesado=0;
+	cantidadDeProductosMenos0Grados=0;
+	contadorDePeso=0;
+	totalPesoTodosLosProductos=0;
+	contador=0;	
+	
+
+	while(respuesta=="si")
+	{
+		do
+		{
+			marca=prompt("ingrese marca");
+		}
+		while(!isNaN(marca));
+
+		do
+		{
+			peso=prompt("ingrese peso");
+			peso=parseInt(peso);
+
+		}
+		while(isNaN(peso)|| peso<1 && peso>100);
+
+		do
+		{
+			temperatura=prompt("ingrese temperatura");
+			temperatura=parseInt(temperatura);
+		}
+		while(isNaN(temperatura)||temperatura<-30 && temperatura>30);
+
+		if (temperatura%0) 
+		{
+			cantidadDeTemperaturaPares++;
+		}
+
+		if (peso>marcaProductoMasPesado) 
+		{
+			marcaProductoMasPesado=marca
+		}
+
+		if (temperatura<0) 
+		{
+			cantidadDeProductosMenos0Grados++;
+		}
+
+
+		if (contador==0 || peso>pesoMaximo)
+		{
+			pesoMaximo=peso
+		}
+		if (contador==0 || peso<pesoMinimo) 
+		{
+			pesoMinimo=peso
+		}
+
+		totalPesoTodosLosProductos=totalPesoTodosLosProductos+peso;
+		contadorDePeso++;
+		contador++;
+
+
+
+		
+
+
+		
+
+
+
+
+
+
+
+		respuesta=prompt("Escriba si si desea continuar");
+	}
+		promedioDelPeso=totalPesoTodosLosProductos/contadorDePeso;
+		document.write("<br>La marca del producto mas pesado es:  "+marcaProductoMasPesado);
+		document.write("<br>La cantidad de temperaturas pares es:   "+cantidadDeTemperaturaPares);
+		document.write("<br>La cantidad de productos que se conservan a -0grados son:   "+cantidadDeProductosMenos0Grados);
+		document.write("<br>El peso maximo es:  "+pesoMaximo);
+		document.write("<br>El peso minimo es:  "+pesoMinimo);
 }
